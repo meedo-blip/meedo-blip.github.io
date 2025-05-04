95021c243f769c6d79400144d627b085d63d80b6
meedo-blip <hamidalramzy8@gmail.com>
2025-05-04 04:02:52 +0300

    fixed menu and resize bugs and added seperate highscores for each difficulty.

--- snake.js
+++ snake.js
@@ -10,13 +10,49 @@ const snakeLen = Math.floor(boardLen / 2);
 const upBtn = document.getElementById("up");
 const leftBtn = document.getElementById("left");
 const downBtn = document.getElementById("down");
-const rightBtn = document.getElementById("right");
+const rightBtn = document.getElementById("right")
 
 let pixelWidth;
 let snakeHeight;
 let snake = [];
 
-class snakePart {
+
+let newDirection = "E"; // N: up, S: down, E: right, W: left
+let myInterval = NaN;
+let myIntervalSpeed = 25;
+let hsInterval = NaN;
+let difficultyId = 'hsEasy';
+let posRotate = [];
+let apple;
+let canPlay = false;
+let step;
+let score = 0;
+let highscore = 0;
+let madeApple = false;
+let victory = false;
+let surpassCount;
+
+let startCount = 0;
+let tickCount = 0;
+
+let soundFiles = [
+    "sounds/food-crunch.wav",
+    "sounds/lose-horror.wav",
+    "sounds/victory.mp3"
+];
+
+const startMenuHeader = '<div id="menu"><div id="header"><h1>Snake Game</h1><img class="emoji" src="images/snake.png"></div>'
+const victoryMenuHeader1 = '<div id="menu"><div id="winheader"><div></div><h1 style="-webkit-text-stroke: 1px #3d3d3d; font-size: 3rem; font-weight: 700;">Victory!</h1><h4>You\'ve surpassed your highscore by'
+const victoryMenuHeader2 = ' apples.</h4></div>';
+const loseMenuHeader = '<div id="menu"><div id="loseheader"><h1 style="-webkit-text-stroke: 1px #3d3d3d; font-size: 3rem; font-weight: 700;">Game Over!</h1></div>'
+
+const menuBody1 = '<div id="difficulty"><label for="difficulty">Difficulty:</label><select name="difficulty" id="select"><option class="option" value="25" style="color:#10bb00;">Easy</option><option class="option" value="20" style="color: yellow;">Normal</option><option class="option" value="10" style="color: #ff4d00;">Hard</option><option class="option" value="5" style="color: red;">Extremly hard</option></select></div><div id="scores"><div class="apple start"><div class="fruit start"></div><div class="stem start"></div></div><div><label for="score">Score:</label><h2 id="score" name="score">';
+const menuBody2 = '</h2></div><img src="images/trophy.png" id="trophy"><div><label for="highscore">HighScore:</label><h2 id="highscore" name="highscore">'; 
+const menuBody3 = '</h2></div></div><button id="startbtn"><h2 id="startbtntext">Start</h2></button></div>';
+
+const angleStep = 90 / 25; 
+
+class SnakePart {
     constructor(index, id) {
         this.element = document.getElementById(id);
         this.top;
@@ -53,12 +89,12 @@ function makeSnake() {
     
     snakeBoard.innerHTML += "<div id='snakeback'></div>";
 
-    snake.push(new snakePart(0, "snakehead"));
+    snake.push(new SnakePart(0, "snakehead"));
 
     for(i=0; i < snakeLen - 2; i++) 
-        snake.push(new snakePart(i+1, "snakebody" + i));
+        snake.push(new SnakePart(i+1, "snakebody" + i));
 
-    snake.push(new snakePart(snakeLen - 1, "snakeback"));
+    snake.push(new SnakePart(snakeLen - 1, "snakeback"));
 
     console.log("hi people");
 }
@@ -84,7 +120,7 @@ function alignSnake() {
     posRotate = [];
 }
 
-function makeMenu(start) {
+function makeMenu(mode) {
     canPlay = false;
 
     const cover = document.createElement("div");
@@ -92,12 +128,18 @@ function makeMenu(start) {
 
     document.body.insertBefore(cover, document.getElementById("buttons").nextSibling);
 
-    if(start){
-        cover.innerHTML ='<div id="menu"><div id="header"><h1>Snake Game</h1><img class="emoji" src="images/snake.png"></div><div id="scores"><div class="apple start"><div class="fruit start"></div><div class="stem start"></div></div><div><label for="score">Score:</label><h2 id="score" name="score">'+ score +'</h2></div><img src="images/trophy.png" id="trophy"><div><label for="highscore">High Score:</label><h2 id="highscore" name="highscore">'+ highscore +'</h2></div></div><div id="difficulty"><label for="difficulty">Difficulty:</label> <select name="difficulty" id="select"><option class="option" value="25" style="color:#10bb00;">Easy</option><option class="option" value="20" style="color: yellow;">Normal</option><option class="option" value="10" style="color: #ff4d00;">Hard</option><option class="option" value="5" style="color: red;">Extremly hard</option></select></div><button id="startbtn"><h2 id="startbtntext">Start</h2></button>';
-    } else if(victory){
-        cover.innerHTML ='<div id="menu"><div id="winheader"><div></div><h1 style="-webkit-text-stroke: 1px #3d3d3d; font-size: 3rem; font-weight: 700;">Victory!</h1><h4>You\'ve surpassed your highscore by '+ surpassCount +' apples.</h4></div><div id="scores"><div class="apple start"><div class="fruit start"></div><div class="stem start"></div></div><div><label for="score">Score:</label><h2 id="score" name="score">'+ score +'</h2></div><img src="images/trophy.png" id="trophy"><div><label for="highscore">High Score:</label><h2 id="highscore" name="highscore">'+ highscore +'</h2></div></div><div id="difficulty"><label for="difficulty">Difficulty:</label> <select name="difficulty" id="select"><option class="option" value="25" style="color:#10bb00;">Easy</option><option class="option" value="20" style="color: yellow;">Normal</option><option class="option" value="10" style="color: #ff4d00;">Hard</option><option class="option" value="5" style="color: red;">Extremly hard</option></select></div><button id="startbtn"><h2 id="startbtntext">Start</h2></button>';
-    } else {
-        cover.innerHTML ='<div id="menu"><div id="loseheader"><h1 style="-webkit-text-stroke: 1px #3d3d3d; font-size: 3rem; font-weight: 700;">Game Over!</h1></div><div id="scores"><div class="apple start"><div class="fruit start"></div><div class="stem start"></div></div><div><label for="score">Score:</label><h2 id="score" name="score">'+ score +'</h2></div><img src="images/trophy.png" id="trophy"><div><label for="highscore">High Score:</label><h2 id="highscore" name="highscore">'+ highscore +'</h2></div></div><div id="difficulty"><label for="difficulty">Difficulty:</label> <select name="difficulty" id="select"><option class="option" value="25" style="color:#10bb00;">Easy</option><option class="option" value="20" style="color: yellow;">Normal</option><option class="option" value="10" style="color: #ff4d00;">Hard</option><option class="option" value="5" style="color: red;">Extremly hard</option></select></div><button id="startbtn"><h2 id="startbtntext">Start</h2></button>';
+    let menuBody = menuBody1 + score + menuBody2 + highscore + menuBody3;
+
+    switch(mode){
+        case 0: // start or restart
+            cover.innerHTML = startMenuHeader + menuBody;
+            break;
+        case 1: // win
+            cover.innerHTML = victoryMenuHeader1 + surpassCount + victoryMenuHeader2 + menuBody;
+            break;
+        case 2: // lose
+            cover.innerHTML = loseMenuHeader + menuBody;
+            break;
     }
 
     document.getElementById("select").addEventListener("change", updateSelection, true);
@@ -117,15 +159,34 @@ function makeMenu(start) {
 function updateSelection(change) {
     const selectDiff = document.getElementById("select");
 
-    if(change) myIntervalSpeed = parseInt(selectDiff.value);
-    else selectDiff.value = String(myIntervalSpeed);
+    if(change) {
+        myIntervalSpeed = parseInt(selectDiff.value);
+        score = 0;
+        document.getElementById('score').innerText = "0";
+    } else 
+        selectDiff.value = String(myIntervalSpeed);
 
-    if(selectDiff.value == "25") selectDiff.style.color = "#10bb00";
-    else if(selectDiff.value == "20") selectDiff.style.color = "yellow";
-    else if(selectDiff.value == "10") selectDiff.style.color = "#ff4d00";
-    else if(selectDiff.value == "5") selectDiff.style.color = "red";
+
+    if(selectDiff.value == "25"){
+        document.getElementById("scores").style.background = selectDiff.style.color = "#10bb00";
+        difficultyId = "hsExtreme"
+    }
+    else if(selectDiff.value == "20"){ 
+        document.getElementById("scores").style.background = selectDiff.style.color = "#f5d109";
+        difficultyId = "hsHard"
+    }
+    else if(selectDiff.value == "10"){ 
+        document.getElementById("scores").style.background = selectDiff.style.color = "#ff4d00";
+        difficultyId = "hsNormal"
+    }
+    else if(selectDiff.value == "5"){ 
+        document.getElementById("scores").style.background = selectDiff.style.color = "red";
+        difficultyId = "hsEasy"
+    }
+
+    load_hs();
+    document.getElementById('highscore').innerText = highscore + ''
 }
-
 function makeSoundPlayers() {
     for(i=0; i<soundFiles.length; i++) {
         const audio = document.createElement("AUDIO");
@@ -201,43 +262,22 @@ function addEventListeners() {
 
 }
 
-let newDirection = "E"; // N: up, S: down, E: right, W: left
-let myInterval;
-let myIntervalSpeed = 25;
-let posRotate = [];
-let apple;
-let canPlay = false;
-let step;
-let score = 0;
-let highscore = 0;
-let madeApple = false;
-let victory = false;
-let surpassCount;
-
-let startCount = 0;
-let tickCount = 0;
-
-let soundFiles = [
-    "sounds/food-crunch.wav",
-    "sounds/lose-horror.wav",
-    "sounds/victory.mp3"
-];
-
-const angleStep = 90 / 25; 
 
 function initialize() {
-    highscore = parseInt(localStorage.getItem("highscore"))
-
-    if(!Number.isSafeInteger(highscore)) {
-        highscore = 0
-        localStorage.setItem("highscore", highscore + "")
-    }
 
     buildPixels();
     makeSnake();
     alignSnake();
     makeSoundPlayers();
-    makeMenu(true);
+    makeMenu(0);
+
+    window.addEventListener("resize", () => {
+        pixelWidth = snakeBoard.offsetHeight / boardLen;
+        step = pixelWidth / 25;
+
+        if(document.getElementById("cover") === null) 
+            stop(true);
+    })
 }
 
 function start() {
@@ -249,6 +289,7 @@ function start() {
     score = 0;
     canPlay = false;
     myInterval = setInterval(move, myIntervalSpeed);
+    hsInterval = setInterval(save_hs, 1000)
 }
 
 function move() {
@@ -259,7 +300,7 @@ function move() {
     const s = Math.round(step / pixelWidth * 100) / 100;
 
     if(x > boardLen + 1|| y > boardLen + 1 || x < 0 || y < 0) {
-        stop();
+        stop(false);
         return; 
     }
 
@@ -480,26 +521,47 @@ function makeApple() {
     }
 }
 
+function load_hs() {
+    highscore = parseInt(localStorage.getItem(difficultyId))
+    if(Number.isNaN(highscore)) {
+        highscore = 0;
+        save_hs()
+    } 
+}
 
-function stop() {
+function save_hs() {
+    localStorage.setItem(difficultyId, highscore + "")
+}
+
+function stop(isRestart) {
     clearInterval(myInterval);
+    clearInterval(hsInterval)
+    myInterval = hsInterval = NaN;
 
     if(score > highscore){
-        victory = true;
-        soundFiles[2].play();
-
         surpassCount = score - highscore;
-
         highscore = score;
-        localStorage.setItem("highscore", String(highscore))
+        save_hs()
+
+        if(isRestart){
+            makeMenu(0)
+            return
+        } 
+        soundFiles[2].play();
+        makeMenu(1);
+        return
     } 
-    else {
-        victory = false;
-        soundFiles[1].play();
-    }
-    
-    makeMenu(false);
+    else if(isRestart){
+            makeMenu(0)
+            return
+        } 
+
+    // lose if not returned
+    soundFiles[1].play();
+    makeMenu(2)
 }
+    
+
 
 // Initial start functions
 initialize();
